3d26a70966851c32faf3bf026a89a0a38c49dc13
Mohammed Eshan <itsrandomboy77@gmail.com>
2026-01-26 21:24:24 +0400

    how did this not get added?

--- src/canvas/matrix_operations.c
+++ src/canvas/matrix_operations.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   matrix_oprations.c                                 :+:      :+:    :+:   */
+/*   matrix_operations.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtangalv <mtangalv@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 19:22:51 by mtangalv          #+#    #+#             */
-/*   Updated: 2026/01/26 19:30:06 by mtangalv         ###   ########.fr       */
+/*   Updated: 2026/01/26 21:13:28 by mtangalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,26 @@ int	matrix4_equal(t_matrix4 a, t_matrix4 b)
 	{
 		j = 0;
 		while (j < 4)
+		{
+			if (!is_equal(a.data[i][j], b.data[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int matrix3_equal(t_matrix3 a, t_matrix3 b)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 3)
+	{
+		j = 0;
+		while (j < 3)
 		{
 			if (!is_equal(a.data[i][j], b.data[i][j]))
 				return (0);
